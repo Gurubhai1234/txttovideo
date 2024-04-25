6c6272ab7797c114752714993e101ee6eb75f81e
Gurubhai1234 <166490236+Gurubhai1234@users.noreply.github.com>
2024-04-25 13:19:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 import os
 
-API_ID = API_ID = Your api id
+API_ID = API_ID = 20352686
 
-API_HASH = os.environ.get("API_HASH", "Your api hash")
+API_HASH = os.environ.get("API_HASH", "d0c3424472358f66d8dbddfdb7017730")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "Your bot token")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6956263556:AAESxqNLatcl7oZ4IEBStspCZvo8HjRK6P8")
 
-PASS_DB = int(os.environ.get("PASS_DB", "100"))
+PASS_DB = int(os.environ.get("PASS_DB", "721"))
 
-OWNER = int(os.environ.get("OWNER", Your user id ))
+OWNER = int(os.environ.get("OWNER",6982484823))
 
 LOG = -1002008011161            #don't change it otherwise you face error while deploying.
 try:
